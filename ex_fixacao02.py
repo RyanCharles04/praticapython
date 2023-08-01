c350fbad3bfc9927887e3e4a9ad9ba14ad9a3c2d
Ryan Charles <ryancharles040808@gmail.com>
2023-08-01 11:37:25 -0300

    Concerto de porcentagem

--- ex_fixacao02.py
+++ ex_fixacao02.py
@@ -1,7 +1,7 @@
 cont = float(input("Qual é o valor da conta?"))
 
-gar = cont * 16/100
+gar = cont * 10/100
 
 val = cont + gar
 
-print("O valor da conta mais o acréscimo de 16% do garçom é", val)
+print("O valor da conta mais o acréscimo de 10% do garçom é", val)
